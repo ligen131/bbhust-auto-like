0d7b77e9d2f55f93fac852c5ebef48917fd71e67
ligen131 <i@ligen131.com>
2023-04-19 17:05:20 +0800

    ✨ feat: auto like every 20 seconds

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S node --no-warnings --loader ts-node/esm
 /**
- *   ts-cli - https://github.com/ligen131/ts-cli
+ *   bbhust-auto-like - https://github.com/ligen131/bbhust-auto-like
  *
  *   @copyright 2022 ligen131 <1353055672@qq.com>
  *
@@ -18,7 +18,108 @@
  *
  */
 'use strict';
+import fetch from 'node-fetch';
+import config from '../config/config.json' assert { type: 'json' };
 
-import { EXPORT_TEST } from './group/group';
+interface userData {
+  data:
+    | {
+        _id: string;
+        expire: number;
+        token: string;
+      }
+    | null
+    | undefined;
+  success: boolean;
+  message: string | undefined;
+}
 
-console.log(EXPORT_TEST);
+interface posts {
+  id: string;
+  title: string;
+  liked: boolean;
+}
+
+interface postData {
+  data:
+    | {
+        getLatestPosts: {
+          posts: posts[];
+        };
+      }
+    | null
+    | undefined;
+}
+
+async function login(): Promise<{
+  success: boolean;
+  message: string | undefined;
+  token: string | undefined;
+}> {
+  const resp = await fetch(
+    `https://api.hust.online/bbhust/api/v1/user/token?person_id=${config.person_id}&password=${config.password}`,
+    {
+      headers: {
+        accept: 'application/json, text/plain, */*',
+      },
+      body: null,
+      method: 'GET',
+    },
+  );
+  const data = (await resp.json()) as userData;
+  return {
+    success: data.success,
+    message: data?.message,
+    token: data?.data?.token,
+  };
+}
+
+async function getLatestPost(token: string | undefined): Promise<posts[]> {
+  const resp = await fetch('https://api.hust.online/bbhust/api/graphql', {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+    body: `{"query":"query{  getLatestPosts(time: \\"999999999999\\", limit: 15) {  posts {    id title liked  }  }  } "}`,
+    method: 'POST',
+  });
+  const data = (await resp.json()) as postData;
+  const arr = data?.data?.getLatestPosts.posts;
+  if (!arr) {
+    return [];
+  }
+  return arr.filter((value) => {
+    return !value.liked;
+  });
+}
+
+async function likeAction(token: string, posts: posts[]) {
+  console.log('Action like.');
+  posts.forEach(async (post) => {
+    await fetch('https://api.hust.online/bbhust/api/graphql', {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+      body: `{"query":"  mutation($id: String!, $liked: Boolean!) {  likePost(id: $id, liked: $liked) {  liked  }  } ","variables":{"id":"${post.id}","liked":true}}`,
+      method: 'POST',
+    });
+    console.log(`Liked post ${post.title}`);
+  });
+}
+
+async function main() {
+  try {
+    const user = await login();
+    if (!user.success) {
+      console.log('Login failed.', user.message);
+      return;
+    }
+    if (user.token) {
+      await likeAction(user.token, await getLatestPost(user.token));
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+main();
+setInterval(main, 20 * 1000);
